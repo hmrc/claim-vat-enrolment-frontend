0f0fdf256a65a05d2d47c1d70cf6e08c8ed0127f
elliot <51033975+elliots41@users.noreply.github.com>
2021-05-12 13:19:54 +0100

    update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,12 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "4.3.0",
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "4.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.59.0-play-27",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.2.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.61.0-play-27",
     "uk.gov.hmrc" %% "play-frontend-govuk" % "0.71.0-play-27",
-    "uk.gov.hmrc" %% "play-language" % "4.12.0-play-27",
+    "uk.gov.hmrc" %% "play-language" % "5.0.0-play-27",
     "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-27",
-    "uk.gov.hmrc" %% "auth-client" % "5.2.0-play-27"
+    "uk.gov.hmrc" %% "auth-client" % "5.6.0-play-27"
   )
 
   val sharedTestDependencies: Seq[ModuleID] = {
@@ -22,13 +21,13 @@ object AppDependencies {
       "com.typesafe.play" %% "play-test" % current % scope,
       "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % scope,
       "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % scope,
-      "org.scoverage" %% "scalac-scoverage-runtime" % "1.4.1" % scope
+      "org.scoverage" %% "scalac-scoverage-runtime" % "1.4.6" % scope
     )
   }
 
   val test = Seq(
-    "org.mockito" % "mockito-core" % "3.6.0" % Test,
-    "org.scalatestplus" %% "mockito-3-4" % "3.2.3.0" % Test
+    "org.mockito" % "mockito-core" % "3.9.0" % Test,
+    "org.scalatestplus" %% "mockito-3-4" % "3.2.8.0" % Test
   )
 
   val it = Seq(

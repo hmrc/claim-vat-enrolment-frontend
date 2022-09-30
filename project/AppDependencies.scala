512f19464292aa050329c14b4f7f1342b84449f8
refaque-ahmad <75069573+refaque-ahmad@users.noreply.github.com>
2022-09-30 14:53:19 +0100

    Updated bootstrap-frontend-play version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.4.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.6.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.28.0-play-28",
     "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-28",
     "uk.gov.hmrc" %% "auth-client" % "5.14.0-play-28"

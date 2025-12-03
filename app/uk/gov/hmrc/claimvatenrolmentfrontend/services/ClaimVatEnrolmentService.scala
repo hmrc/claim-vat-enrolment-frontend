bf215f4ca433fe576820e1bb697eeeb527b7d479
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2025-12-03 15:32:01 +0000

    DL-17816 : CVE Lock Logic
    # Conflicts:
    #	app/uk/gov/hmrc/claimvatenrolmentfrontend/config/AppConfig.scala
    #	app/uk/gov/hmrc/claimvatenrolmentfrontend/models/JourneySubmission.scala
    #	app/uk/gov/hmrc/claimvatenrolmentfrontend/repositories/JourneySubmissionRepository.scala
    #	app/uk/gov/hmrc/claimvatenrolmentfrontend/services/ClaimVatEnrolmentService.scala

--- app/uk/gov/hmrc/claimvatenrolmentfrontend/services/ClaimVatEnrolmentService.scala
+++ app/uk/gov/hmrc/claimvatenrolmentfrontend/services/ClaimVatEnrolmentService.scala
@@ -18,9 +18,11 @@ package uk.gov.hmrc.claimvatenrolmentfrontend.services
 
 import play.api.data.Form
 import play.api.mvc.Request
+import uk.gov.hmrc.claimvatenrolmentfrontend.config.AppConfig
 import uk.gov.hmrc.claimvatenrolmentfrontend.connectors.AllocateEnrolmentConnector.etmpDateFormat
 import uk.gov.hmrc.claimvatenrolmentfrontend.httpparsers.QueryUsersHttpParser.{NoUsersFound, UsersFound}
 import uk.gov.hmrc.claimvatenrolmentfrontend.models._
+import uk.gov.hmrc.claimvatenrolmentfrontend.repositories.JourneySubmissionRepository
 import uk.gov.hmrc.claimvatenrolmentfrontend.services.ClaimVatEnrolmentService._
 import uk.gov.hmrc.http.{HeaderCarrier, InternalServerException}
 import uk.gov.hmrc.play.audit.AuditExtensions

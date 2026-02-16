dd4dab2cd9e37c85539dc759ce1b2aa5233746a0
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2026-02-16 16:37:03 +0000

    Fixing the review comments

--- test/uk/gov/hmrc/claimvatenrolmentfrontend/models/VatKnownFactsSpec.scala
+++ test/uk/gov/hmrc/claimvatenrolmentfrontend/models/VatKnownFactsSpec.scala
@@ -19,12 +19,15 @@ package uk.gov.hmrc.claimvatenrolmentfrontend.models
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.libs.json._
+import uk.gov.hmrc.claimvatenrolmentfrontend.repositories.JourneyDataRepository
+import uk.gov.hmrc.claimvatenrolmentfrontend.repositories.JourneyDataRepository._
 
 import java.time.{LocalDate, Month}
 
 class VatKnownFactsSpec extends PlaySpec with GuiceOneAppPerSuite with MockitoSugar {
 
-  "VatKnownFacts.hasCompleteData" should {
+  "ReturnsInformation.hasCompleteData" should {
 
     "return true when both boxFive and lastReturnMonth are defined" in {
       ReturnsInformation(Some("1000"), Some(Month.JANUARY)).hasCompleteData mustBe true
@@ -45,7 +48,7 @@ class VatKnownFactsSpec extends PlaySpec with GuiceOneAppPerSuite with MockitoSu
 
   "VatKnownFacts.hasCompleteJourneyData" should {
 
-    def mockVatKnownFacts(
+    def buildVatKnownFacts(
                            vatRegDate: Option[LocalDate],
                            optRetInfo: Option[ReturnsInformation],
                            formBundleRef: Option[String]
@@ -58,31 +61,85 @@ class VatKnownFactsSpec extends PlaySpec with GuiceOneAppPerSuite with MockitoSu
         formBundleReference    = formBundleRef
       )
 
-    "return false when date is not defined (regardless of returnInfo/formBundleRef)" in {
-      mockVatKnownFacts(None, Some(ReturnsInformation(Some("1000"), Some(Month.JANUARY))), None).hasCompleteJourneyData mustBe false
-      mockVatKnownFacts(None, None, Some("ref")).hasCompleteJourneyData mustBe false
-      mockVatKnownFacts(None, Some(ReturnsInformation(None, None)), Some("ref")).hasCompleteJourneyData mustBe false
+    "return 'true'" when {
+      "date is defined and returnInfo has complete data" in {
+        val returnInfo = ReturnsInformation(Some("1000"), Some(Month.FEBRUARY))
+        buildVatKnownFacts(Some(LocalDate.now), Some(returnInfo), None).hasCompleteJourneyData mustBe true
+      }
+      "date is defined and returnInfo is incomplete but formBundleRef is defined" in {
+        val incompleteReturnInfo = ReturnsInformation(Some("1000"), None) // incomplete
+        buildVatKnownFacts(Some(LocalDate.now), Some(incompleteReturnInfo), Some("opt-ref")).hasCompleteJourneyData mustBe true
+      }
+      "date is defined, returnInfo is absent, and formBundleRef is defined" in {
+        buildVatKnownFacts(Some(LocalDate.now), None, Some("opt-ref")).hasCompleteJourneyData mustBe true
+      }
     }
 
-    "return true when date is defined and returnInfo has complete data" in {
-      val returnInfo = ReturnsInformation(Some("1000"), Some(Month.FEBRUARY))
-      mockVatKnownFacts(Some(LocalDate.now), Some(returnInfo), None).hasCompleteJourneyData mustBe true
-    }
+    "return 'false'" when {
+      "date is not defined (regardless of returnInfo/formBundleRef)" in {
+        buildVatKnownFacts(None, Some(ReturnsInformation(Some("1000"), Some(Month.JANUARY))), None).hasCompleteJourneyData mustBe false
+        buildVatKnownFacts(None, None, Some("ref")).hasCompleteJourneyData mustBe false
+        buildVatKnownFacts(None, Some(ReturnsInformation(None, None)), Some("ref")).hasCompleteJourneyData mustBe false
+      }
+      "date is defined but both returnInfo is incomplete/absent and formBundleRef is not defined" in {
+        val incompleteReturnInfo = ReturnsInformation(None, Some(Month.MARCH)) // incomplete
+        buildVatKnownFacts(Some(LocalDate.now), Some(incompleteReturnInfo), None).hasCompleteJourneyData mustBe false
 
-    "return true when date is defined and returnInfo is incomplete but formBundleRef is defined" in {
-      val incompleteReturnInfo = ReturnsInformation(Some("1000"), None) // incomplete
-      mockVatKnownFacts(Some(LocalDate.now), Some(incompleteReturnInfo), Some("opt-ref")).hasCompleteJourneyData mustBe true
-    }
-
-    "return true when date is defined, returnInfo is absent, and formBundleRef is defined" in {
-      mockVatKnownFacts(Some(LocalDate.now), None, Some("opt-ref")).hasCompleteJourneyData mustBe true
-    }
-
-    "return false when date is defined but both returnInfo is incomplete/absent and formBundleRef is not defined" in {
-      val incompleteReturnInfo = ReturnsInformation(None, Some(Month.MARCH)) // incomplete
-      mockVatKnownFacts(Some(LocalDate.now), Some(incompleteReturnInfo), None).hasCompleteJourneyData mustBe false
-
-      mockVatKnownFacts(Some(LocalDate.now), None, None).hasCompleteJourneyData mustBe false
+        buildVatKnownFacts(Some(LocalDate.now), None, None).hasCompleteJourneyData mustBe false
+      }
     }
   }
+
+  "check the json reads of vatKnownFacts" in {
+    val json = Json.parse(
+      s"""
+         |{
+         |    "vatNumber": "123123123",
+         |    "vatRegistrationDate": "2021-01-01",
+         |    "submittedVatReturn": true,
+         |    "box5Figure": "1000.00",
+         |    "lastMonthSubmitted": 1
+         |}
+         """.stripMargin)
+
+    val expectedVatKnownFacts = VatKnownFacts(
+      vatNumber = "123123123",
+      vatRegistrationDate = Some(LocalDate.of(2021, 1, 1)),
+      optPostcode = None,
+      optReturnsInformation = Some(ReturnsInformation(
+        boxFive = Some("1000.00"),
+        lastReturnMonth = Some(Month.JANUARY))),
+      formBundleReference = None
+    )
+
+    JourneyDataRepository.vatKnownFactsReads.reads(json) mustBe JsSuccess(expectedVatKnownFacts)
+  }
+
+  "check the json writes of vatKnownFacts" in {
+    val vatKnownFacts = VatKnownFacts(
+      vatNumber = "321321321",
+      vatRegistrationDate = Some(LocalDate.of(2022, 1, 1)),
+      optPostcode = Some(Postcode("AA1 1AA")),
+      optReturnsInformation = Some(ReturnsInformation(
+        boxFive = Some("1250.00"),
+        lastReturnMonth = Some(Month.FEBRUARY))),
+      formBundleReference = None
+    )
+
+    val expectedJson = Json.parse(
+      s"""
+         |{
+         |    "vatNumber": "321321321",
+         |    "vatRegistrationDate": "2022-01-01",
+         |    "vatRegPostcode": "AA1 1AA",
+         |    "submittedVatReturn": true,
+         |    "box5Figure": "1250.00",
+         |    "lastMonthSubmitted": 2,
+         |    "formBundleReference": null
+         |}
+         """.stripMargin)
+
+    Json.toJson(vatKnownFacts) mustBe expectedJson
+  }
+
 }
